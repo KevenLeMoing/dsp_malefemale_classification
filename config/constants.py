1a4f1b977c4adb28c3d3cda58723f017df43f703
KevenLeMoing <keven@fundshop.fr>
2018-03-11 22:50:11 +0100

    Misc

---
+++ config/constants.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# EXAMPLE
+my_const_1 = "blabla"
+my_const_2 = 0000
+url = "http://blablabla"
---
+++ config/parameters.py
@@ -0,0 +1,17 @@
+# MySQL
+mysql_db_name = ""
+mysql_db_host = ""
+mysql_db_port = 3306
+
+# MongoDB
+mongo_db_name = ""
+mongo_db_host = ""
+mongo_db_port = 27017
+
+# dataprovider_EXAMPLE
+dataprovider_login = ""
+dataprovider_pass = ""
+dataprovider_api_url = "http://blabla:8080"
+dataprovider_api_key = ""
+databrovider_api_secret = ""
+
