5cb77a3ac3147fe9ad51a55f49ec5636421553e7
KevenLeMoing <keven@fundshop.fr>
2018-04-16 21:13:37 +0200

    anonimzation

--- config/constants.py
+++ config/constants.py
@@ -2,7 +2,7 @@
 voxforge_url = "http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit/"
 
 # data_path
-downloads_path = '/Users/kevenlemoing/Sites/sandvik_code_assignement/data/downloads/'
-scraping_result_path = '/Users/kevenlemoing/Sites/sandvik_code_assignement/data/scraping_result.csv'
-scraping_result__sliced_path = '/Users/kevenlemoing/Sites/sandvik_code_assignement/data/scraping_result_sliced.csv'
-indicators_path = '/Users/kevenlemoing/Sites/sandvik_code_assignement/data/indicators.csv'
+downloads_path = '/Users/kevenlemoing/Sites/dsp_malefemale_classification/data/downloads/'
+scraping_result_path = '/Users/kevenlemoing/Sites/dsp_malefemale_classification/data/scraping_result.csv'
+scraping_result__sliced_path = '/Users/kevenlemoing/Sites/dsp_malefemale_classification/data/scraping_result_sliced.csv'
+indicators_path = '/Users/kevenlemoing/Sites/dsp_malefemale_classification/data/indicators.csv'
